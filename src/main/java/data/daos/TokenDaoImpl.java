d6ccd7dc458479714734351e009367511d5539e8
Jorgelou <sirjorgete@gmail.com>
2016-03-28 23:52:56 +0200

    findByValue function needed in Token DAO

--- src/main/java/data/daos/TokenDaoImpl.java
+++ src/main/java/data/daos/TokenDaoImpl.java
@@ -14,7 +14,7 @@ public class TokenDaoImpl implements TokenExtended {
 	@Override
 	public void deleteExpiredTokens() {
 		List<Token> allTokens = tokenDao.findAll();
-		for (Token t:allTokens) {			
+		for (Token t:allTokens) {	
 			if (t.isTokenExpired()) tokenDao.delete(t);
 		}	
 	}
