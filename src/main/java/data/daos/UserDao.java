34a9524b6944d15e43f9582522754d06725d2a1a
Jorgelou <sirjorgete@gmail.com>
2016-03-28 23:52:31 +0200

    findByTokenValue overrides in user DAO for checking token's expiration validity

--- src/main/java/data/daos/UserDao.java
+++ src/main/java/data/daos/UserDao.java
@@ -3,12 +3,10 @@ package data.daos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import data.entities.Training;
 import data.entities.User;
 
-public interface UserDao extends JpaRepository<User, Integer> {
-    
-    @Query("select token.user from Token token where token.value = ?1")
-    public User findByTokenValue(String tokenValue);
+public interface UserDao extends JpaRepository<User, Integer>, UserDaoExtended {
 
     @Query("select user from User user where user.username = ?1 or user.email = ?1")
     public User findByUsernameOrEmail(String id);
